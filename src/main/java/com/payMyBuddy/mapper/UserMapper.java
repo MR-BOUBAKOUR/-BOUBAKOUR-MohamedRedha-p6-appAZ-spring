8532f32adf5b4b379c04438422cedbf4589417db
MR-BOUBAKOUR <redha.boubakour@gmail.com>
2025-02-19 16:07:14 +0100

    removing some unused lines on the UserMapper

--- src/main/java/com/payMyBuddy/mapper/UserMapper.java
+++ src/main/java/com/payMyBuddy/mapper/UserMapper.java
@@ -3,17 +3,12 @@ package com.payMyBuddy.mapper;
 import com.payMyBuddy.dto.user.UserResponseDTO;
 import com.payMyBuddy.model.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.Optional;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(target = "id", source = "user.id")
-    @Mapping(target = "username", source = "user.username")
-    @Mapping(target = "email", source = "user.email")
-    @Mapping(target = "balance", source = "user.balance")
     default Optional<UserResponseDTO> toResponseDTO(Optional<User> user) {
         if (user.isEmpty()) {
             return Optional.empty();
